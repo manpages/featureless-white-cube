11ad7e0043a26a7df5625f69ee01f372d509591e
Jonn Mostovoy <amarr.industrial@gmail.com>
2013-04-18 18:37:39 +0300

    302 works properly

--- lib/http.ex
+++ lib/http.ex
@@ -1,9 +1,16 @@
 defmodule Fwc.HTTP do
   defmodule Balances do
-    def init(_, req, []), do: {:ok, req, :undefined}
-    def handle(req, state) do 
+    def init(_, req, []) do
+      {path, req} = :cowboy_req.path req
+      {:ok, req, path}
+    end
+    def handle(req, "/balances/") do 
       {:ok, {Fwc.Balances, uuid, pid}} = Fwc.Sup.add_game Fwc.Balances
       {:ok, req} = :cowboy_req.reply 302, [{"Location", <<"/balances/", uuid :: binary>>}], "", req 
+      {:ok, req, "/balances/"}
+    end
+    def handle(req, state) do
+      {:ok, req} = :cowboy_req.reply 200, [], "THE GAME.", req
       {:ok, req, state}
     end
     def terminate(_, _, _), do: :ok
